2a6eec03c80afdb586b37593967ab24c270a8ccf
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2016-10-14 08:19:12 -0400

    Add tcp info to JSON

--- tcp_json_builder.c
+++ tcp_json_builder.c
@@ -87,10 +87,14 @@ json_t *build_sock_opened_ev(TcpEvSockOpened *ev)
 {
 	json_t *json_ev = json_object();
 	build_shared_fields(json_ev, (TcpEvent *) ev);
-	json_object_set_new(json_ev, "sockCloexec", 
+
+	json_t *json_details = json_object();
+	json_object_set_new(json_details, "sockCloexec", 
 			json_boolean(ev->sock_cloexec));
-	json_object_set_new(json_ev, "sockNonblock", 
+	json_object_set_new(json_details, "sockNonblock", 
 			json_boolean(ev->sock_nonblock));
+	json_object_set_new(json_ev, "details", json_details);
+	
 	return json_ev;
 }
 
@@ -98,6 +102,10 @@ json_t *build_sock_closed_ev(TcpEvSockClosed *ev)
 {
 	json_t *json_ev = json_object();
 	build_shared_fields(json_ev, (TcpEvent *) ev);
+
+	json_t *json_details = json_object();
+	json_object_set_new(json_ev, "details", json_details);
+
 	return json_ev;
 }
 
@@ -105,7 +113,11 @@ json_t *build_data_sent_ev(TcpEvDataSent *ev)
 {
 	json_t *json_ev = json_object();
 	build_shared_fields(json_ev, (TcpEvent *) ev);
-	json_object_set_new(json_ev, "bytes", json_integer(ev->bytes));
+
+	json_t *json_details = json_object();
+	json_object_set_new(json_details, "bytes", json_integer(ev->bytes));
+	json_object_set_new(json_ev, "details", json_details);
+
 	return json_ev;
 }
 
@@ -113,7 +125,11 @@ json_t *build_data_received_ev(TcpEvDataReceived *ev)
 {
 	json_t *json_ev = json_object();
 	build_shared_fields(json_ev, (TcpEvent *) ev);
-	json_object_set_new(json_ev, "bytes", json_integer(ev->bytes));
+
+	json_t *json_details = json_object();
+	json_object_set_new(json_details, "bytes", json_integer(ev->bytes));
+	json_object_set_new(json_ev, "details", json_details);
+
 	return json_ev;
 }
 
@@ -121,6 +137,10 @@ json_t *build_connected_ev(TcpEvConnected *ev)
 {
 	json_t *json_ev = json_object();
 	build_shared_fields(json_ev, (TcpEvent *) ev);
+
+	json_t *json_details = json_object();
+	json_object_set_new(json_ev, "details", json_details);
+
 	return json_ev;
 }
 
@@ -128,6 +148,53 @@ json_t *build_info_dump_ev(TcpEvInfoDump *ev)
 {
 	json_t *json_ev = json_object();
 	build_shared_fields(json_ev, (TcpEvent *) ev);
+
+	struct tcp_info i = ev->info;
+	json_t *json_details = json_object();
+
+	json_object_set_new(json_details, "state", json_integer(i.tcpi_state));
+	json_object_set_new(json_details, "ca_state", json_integer(i.tcpi_ca_state));
+	json_object_set_new(json_details, "retransmits", json_integer(i.tcpi_retransmits));
+	json_object_set_new(json_details, "probes", json_integer(i.tcpi_probes));
+	json_object_set_new(json_details, "backoff", json_integer(i.tcpi_backoff));
+	json_object_set_new(json_details, "options", json_integer(i.tcpi_options));
+	json_object_set_new(json_details, "snd_wscale", json_integer(i.tcpi_snd_wscale));
+	json_object_set_new(json_details, "rcv_wscale", json_integer(i.tcpi_rcv_wscale));
+
+	json_object_set_new(json_details, "rto", json_integer(i.tcpi_rto));
+	json_object_set_new(json_details, "ato", json_integer(i.tcpi_ato));
+	json_object_set_new(json_details, "snd_mss", json_integer(i.tcpi_snd_mss));
+	json_object_set_new(json_details, "rcv_mss", json_integer(i.tcpi_rcv_mss));
+
+	json_object_set_new(json_details, "unacked", json_integer(i.tcpi_unacked));
+	json_object_set_new(json_details, "sacked", json_integer(i.tcpi_sacked));
+	json_object_set_new(json_details, "lost", json_integer(i.tcpi_lost));
+	json_object_set_new(json_details, "retrans", json_integer(i.tcpi_retrans));
+	json_object_set_new(json_details, "fackets", json_integer(i.tcpi_fackets));
+
+	/* Times */
+	json_object_set_new(json_details, "last_data_sent", json_integer(i.tcpi_last_data_sent));
+	json_object_set_new(json_details, "last_ack_sent", json_integer(i.tcpi_last_ack_sent));
+	json_object_set_new(json_details, "last_data_recv", json_integer(i.tcpi_last_data_recv));
+	json_object_set_new(json_details, "last_ack_recv", json_integer(i.tcpi_last_ack_recv));
+	
+	/* Metrics */
+	json_object_set_new(json_details, "pmtu", json_integer(i.tcpi_pmtu));
+	json_object_set_new(json_details, "rcv_ssthresh", json_integer(i.tcpi_rcv_ssthresh));
+	json_object_set_new(json_details, "rtt", json_integer(i.tcpi_rtt));
+	json_object_set_new(json_details, "rttvar", json_integer(i.tcpi_rttvar));
+	json_object_set_new(json_details, "snd_ssthresh", json_integer(i.tcpi_snd_ssthresh));
+	json_object_set_new(json_details, "snd_cwnd", json_integer(i.tcpi_snd_cwnd));
+	json_object_set_new(json_details, "advmss", json_integer(i.tcpi_advmss));
+	json_object_set_new(json_details, "reordering", json_integer(i.tcpi_reordering));
+
+	json_object_set_new(json_details, "rcv_rtt", json_integer(i.tcpi_rcv_rtt));
+	json_object_set_new(json_details, "rcv_space", json_integer(i.tcpi_rcv_space));
+
+	json_object_set_new(json_details, "total_retrans", json_integer(i.tcpi_total_retrans));
+	
+	json_object_set_new(json_ev, "details", json_details);
+
 	return json_ev;
 }
 
