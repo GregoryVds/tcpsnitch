3403ba5b9c197ecbdf7150563f3c05a5e0959833
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2016-12-22 02:59:01 +0100

    Write basic proportion handler

--- analyzer/proportion_handler.rb
+++ analyzer/proportion_handler.rb
@@ -1,5 +1,18 @@
 class ProportionHandler
+  @@count = 0
+  @@hash = Hash.new(0)
+
   def initialize(init)
-    puts init
+    @@count += 1
+    @@hash[init] += 1
   end
+
+  def self.print
+    @@hash.sort_by { |val, count| -count }.each do |val, count|
+      pc = ((count.to_f/@@count) * 100).round(1)
+      puts "#{val}".ljust(20) + "#{pc}%".ljust(7) + "(#{count})"
+    end
+    puts "TOTAL".ljust(20) + "100%".ljust(7) + "(#{@@count})" 
+  end
+
 end
