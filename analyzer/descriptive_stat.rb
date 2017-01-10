f426d1d51525300ec9e284e098cf440752fd1c91
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2017-01-10 13:19:51 +0100

    Improve complexity of CDF computation

--- analyzer/descriptive_stat.rb
+++ analyzer/descriptive_stat.rb
@@ -18,26 +18,28 @@ class DescriptiveStat
     @@data.descriptive_statistics.each do |key, value|
       puts "#{key}".ljust(20) + "#{value}" 
     end
+    
+    # Only plot CDF is we have a range
+    return unless @@data.range > 0 
 
-		# Only plot CDF is we have a range
-		return unless @@data.range > 0 
+    x = @@data.sort
+    n = x.size
+    y = x.map do |el|
+      ((x.rindex { |v| v <= el } || -1.0) + 1.0) / n * 100.0
+    end
 
-		x = @@data.sort
-		y = x.map { |val| x.percentile_rank(val) }	
-
-		Gnuplot.open do |gp|
-  		Gnuplot::Plot.new(gp) do |plot|
-				plot.xrange "[#{@@data.min}:#{@@data.max}]; set logscale x"
-				plot.title  "CDF for #{options.node_path} (#{options.filter} events)"
-				plot.xlabel "Value"
-				plot.ylabel "Normal CDF"
-			
-				plot.data << Gnuplot::DataSet.new([x,y]) do |ds|
-					ds.with = "lines"
-					ds.linewidth = 4
-					ds.title = options.node_path.split('.').last
-				end
-			end
-  	end
+    Gnuplot.open do |gp|
+      Gnuplot::Plot.new(gp) do |plot|
+        plot.xrange "[#{@@data.min}:#{@@data.max}]; set logscale x"
+        plot.title  "CDF for #{options.node_path} (#{options.filter} events)"
+        plot.xlabel "Value"
+        plot.ylabel "Normal CDF"
+        plot.data << Gnuplot::DataSet.new([x,y]) do |ds|
+          ds.with = "lines"
+          ds.linewidth = 4
+          ds.title = options.node_path.split('.').last
+        end
+      end
+    end
   end
 end
