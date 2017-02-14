bba8a40db23daadc3e088ee6f2cf94255a22483f
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2017-02-14 03:50:27 +0100

    Add tests for dup, dup2, dup3

---
+++ tests/c_programs/dup2_dgram.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <sys/unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void) {
+  int sock;
+  if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    return(EXIT_FAILURE);
+
+  if (dup2(sock, 42) < 0)
+    return(EXIT_FAILURE);
+          
+  return(EXIT_SUCCESS);
+}
---
+++ tests/c_programs/dup3.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <sys/unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void) {
+  int sock;
+  if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    return(EXIT_FAILURE);
+
+  if (dup3(sock, 42, O_CLOEXEC) < 0)
+    return(EXIT_FAILURE);
+          
+  return(EXIT_SUCCESS);
+}
