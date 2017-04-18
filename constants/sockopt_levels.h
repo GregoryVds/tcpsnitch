f3e9122175d19f52f21803abed5f6788d4a4dffc
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2017-04-18 08:37:59 +0200

    Restore ifdef guards for constants

--- constants/sockopt_levels.h
+++ constants/sockopt_levels.h
@@ -13,27 +13,57 @@
 
 static const IntStrPair SOCKOPT_LEVELS[] = {
     // /usr/include/asm-generic/socket.h
+#ifdef SOL_SOCKET
     ADD(SOL_SOCKET),
+#endif
     // /usr/include/netinet/tcp.h
+#ifdef SOL_TCP
     ADD(SOL_TCP),
+#endif
     // /usr/include/netinet/udp.h
+#ifdef SOL_UDP
     ADD(SOL_UDP),
+#endif
     // /usr/include/x86_64-linux-gnu/bits/in.h
+#ifdef SOL_IP
     ADD(SOL_IP),
+#endif
+#ifdef SOL_IPV6
     ADD(SOL_IPV6),
+#endif
+#ifdef SOL_ICMPV6
     ADD(SOL_ICMPV6),
+#endif
     // /usr/include/x86_64-linux-gnu/bits/socket.h
+#ifdef SOL_RAW
     ADD(SOL_RAW),
+#endif
+#ifdef SOL_DECNET
     ADD(SOL_DECNET),
+#endif
+#ifdef SOL_X25
     ADD(SOL_X25),
+#endif
+#ifdef SOL_PACKET
     ADD(SOL_PACKET),
+#endif
+#ifdef SOL_ATM
     ADD(SOL_ATM),
+#endif
+#ifdef SOL_AAL
     ADD(SOL_AAL),
+#endif
+#ifdef SOL_IRDA
     ADD(SOL_IRDA),
+#endif
     // /usr/include/netatalk/at.h
+#ifdef SOL_ATALK
     ADD(SOL_ATALK),
+#endif
     // /usr/include/netrose/rose.h
+#ifdef SOL_ROSE
     ADD(SOL_ROSE),
+#endif
     // /usr/include/netrose/netrom.h
 #ifdef SOL_NETROM
     ADD(SOL_NETROM),
@@ -47,14 +77,24 @@ static const IntStrPair SOCKOPT_LEVELS[] = {
     ADD(SOL_IPX),
 #endif
     // /usr/include/linux/tipc.h
+#ifdef SOL_TIPC
     ADD(SOL_TIPC),
+#endif
+#ifdef SOL_CAN_BASE
     // /usr/include/linux/can.h
     ADD(SOL_CAN_BASE),
+#endif
     // /usr/include/linux/can/raw.h
+#ifdef SOL_CAN_RAW
     ADD(SOL_CAN_RAW),
+#endif
     // /usr/include/linux/irda.h
+#ifdef SOL_IRLMP
     ADD(SOL_IRLMP),
+#endif
+#ifdef SOL_IRTTP
     ADD(SOL_IRTTP),
+#endif
     // /usr/include/linux/rds.h
 #ifdef SOL_RDS
     ADD(SOL_RDS)
--- constants/sol_raw_options.h
+++ constants/sol_raw_options.h
@@ -1,6 +1,8 @@
 #include <linux/icmp.h>
 
 static const IntStrPair SOL_RAW_OPTIONS[] = {
+#ifdef ICMP_FILTER
     ADD(ICMP_FILTER),
+#endif
 #include "_sol_ip_options.h"
 };
